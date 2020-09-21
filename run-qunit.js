4966b13e89d3b7fb12b56834ee72c1e900c3b401
Yong Mu <ym@discoverelement.com>
2020-09-21 15:46:28 -0400

    add junit report

--- run-qunit.js
+++ run-qunit.js
@@ -1,77 +1,37 @@
-"use strict";
 var system = require('system');
-
-/**
- * Wait until the test condition is true or a timeout occurs. Useful for waiting
- * on a server response or for a ui change (fadeIn, etc.) to occur.
- *
- * @param testFx javascript condition that evaluates to a boolean,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param onReady what to do when testFx condition is fulfilled,
- * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
- * as a callback function.
- * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
- */
-function waitFor(testFx, onReady, timeOutMillis) {
-    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3001, //< Default Max Timout is 3s
-        start = new Date().getTime(),
-        condition = false,
-        interval = setInterval(function() {
-            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
-                // If not time-out yet and condition not yet fulfilled
-                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
-            } else {
-                if(!condition) {
-                    // If condition still not fulfilled (timeout but condition is 'false')
-                    console.log("'waitFor()' timeout");
-                    phantom.exit(1);
-                } else {
-                    // Condition fulfilled (timeout and/or condition is 'true')
-                    console.log("'waitFor()' finished in " + (new Date().getTime() - start) + "ms.");
-                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
-                    clearInterval(interval); //< Stop this interval
-                }
-            }
-        }, 100); //< repeat check every 250ms
-};
-
-
-if (system.args.length !== 2) {
-    console.log('Usage: run-qunit.js URL');
-    phantom.exit(1);
+var fs = require('fs');
+var page = require('webpage').create();
+// argument 0 is always the file which is called (this)
+if (system.args.length === 1) {
+    console.log('Pass the path/to/testfile.js as argument to run the test.');
+    phantom.exit();
+} else {
+    // path is relative to where phantomjs is started
+    var url = system.args[1]; // e.g. 'test/unit/tests.html'
+    console.log("Opening " + url);
 }
 
-var page = require('webpage').create();
-
-// Route "console.log()" calls from within the Page context to the main Phantom context (i.e. current "this")
-page.onConsoleMessage = function(msg) {
-    console.log(msg);
+page.onConsoleMessage = function(msg, lineNum, sourceId) {
+  console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
 };
 
-page.open(system.args[1], function(status){
-    if (status !== "success") {
-        console.log("Unable to access network");
-        phantom.exit(1);
+page.open(url, function (status) {
+    console.log("Status: " + status);
+    if (status === "success") {
+        setTimeout(function () {
+            var path = 'results.xml';
+            var output = page.evaluate(function () {
+                console.log("in evaluate");
+                console.log(document.output);
+                return document.output;
+            });
+            fs.write(path, output, 'w');
+            console.log("Wrote JUnit style output of QUnit tests into " + path);
+            console.log("Tests finished. Exiting.");
+            phantom.exit();
+        }, 100);
     } else {
-        waitFor(function(){
-            return page.evaluate(function(){
-                var el = document.getElementById('qunit-testresult');
-                if (el && el.innerText.match('completed')) {
-                    return true;
-                }
-                return false;
-            });
-        }, function(){
-            var failedNum = page.evaluate(function(){
-                var el = document.getElementById('qunit-testresult');
-                console.log(el.innerText);
-                try {
-                    return el.getElementsByClassName('failed')[0].innerHTML;
-                } catch (e) { }
-                return 10000;
-            });
-            phantom.exit((parseInt(failedNum, 10) > 0) ? 1 : 0);
-        });
+        console.log("Failure opening" + url + ". Exiting.");
+        phantom.exit();
     }
 });
